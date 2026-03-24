211bb1d57c3304be62a32459e015ec83d037f40b
Raghul-Saba <rs2298@srmist.edu.in>
2026-03-24 09:19:12 +0530

    Completed UC4

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 abstract class Room {
     protected int numberOfBeds;
     protected int squareFeet;
@@ -16,45 +15,42 @@ abstract class Room {
     }
 }
 class SingleRoom extends Room {
-    int available;
 
-    SingleRoom(int available) {
-        super(1, 250, 1500);
-        this.available = available;
+    SingleRoom() {
+        super(1, 250, 1500.0);
     }
+
+    @Override
     public void displayRoomDetails() {
         System.out.println("Single Room:");
         super.displayRoomDetails();
-        System.out.println("Available: " + available + "\n");
     }
 }
 class DoubleRoom extends Room {
-    int available;
 
-    DoubleRoom(int available) {
-        super(2, 400, 2500);
-        this.available = available;
+    DoubleRoom() {
+        super(2, 400, 2500.0);
     }
+
+    @Override
     public void displayRoomDetails() {
         System.out.println("Double Room:");
         super.displayRoomDetails();
-        System.out.println("Available: " + available + "\n");
     }
 }
 class SuiteRoom extends Room {
-    int available;
 
-    SuiteRoom(int available) {
-        super(3, 750, 5000);
-        this.available = available;
+    SuiteRoom() {
+        super(3, 750, 5000.0);
     }
+    @Override
     public void displayRoomDetails() {
         System.out.println("Suite Room:");
         super.displayRoomDetails();
-        System.out.println("Available: " + available + "\n");
     }
 }
 class RoomInventory {
+
     private Map<String, Integer> roomAvailability;
     public RoomInventory() {
         roomAvailability = new HashMap<>();
@@ -67,33 +63,46 @@ class RoomInventory {
     public Map<String, Integer> getRoomAvailability() {
         return roomAvailability;
     }
-    public void updateAvailability(String roomType, int count) {
-        roomAvailability.put(roomType, count);
+}
+class RoomSearchService {
+    private RoomInventory inventory;
+    RoomSearchService(RoomInventory inventory) {
+        this.inventory = inventory;
     }
-    public void displayInventory() {
-        System.out.println("Current Room Inventory:");
-        for (Map.Entry<String, Integer> entry : roomAvailability.entrySet()) {
-            System.out.println(entry.getKey() + " Rooms Available: " + entry.getValue());
+    public void searchAvailableRooms() {
+
+        Map<String, Integer> availability = inventory.getRoomAvailability();
+        for (Map.Entry<String, Integer> entry : availability.entrySet()) {
+            String type = entry.getKey();
+            int count = entry.getValue();
+
+            if (count > 0) {
+                Room room = null;
+                if (type.equals("Single"))
+                    room = new SingleRoom();
+                else if (type.equals("Double"))
+                    room = new DoubleRoom();
+                else if (type.equals("Suite"))
+                    room = new SuiteRoom();
+                if (room != null) {
+                    room.displayRoomDetails();
+                    System.out.println("Available Rooms: " + count);
+                    System.out.println();
+                }
+            }
         }
     }
 }
 public class BookMyStayApp {
 
     public static void main(String[] args) {
-
-        System.out.println("Welcome user");
+        System.out.println("Welcome User");
         System.out.println("Book My Stay App");
-        System.out.println("Version: 3.0");
-        System.out.println("Hotel Room Initialization\n");
-        SingleRoom s = new SingleRoom(5);
-        DoubleRoom d = new DoubleRoom(3);
-        SuiteRoom p = new SuiteRoom(2);
-        s.displayRoomDetails();
-        d.displayRoomDetails();
-        p.displayRoomDetails();
+        System.out.println("Version: 4.0");
+        System.out.println("Room Search System");
         RoomInventory inventory = new RoomInventory();
         inventory.initializeInventory();
-        System.out.println("\nCentralized Inventory State:");
-        inventory.displayInventory();
+        RoomSearchService searchService = new RoomSearchService(inventory);
+        searchService.searchAvailableRooms();
     }
 }
